84406bf217bb78c2b4442d84f9daa96fd47769ff
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2020-04-20 12:10:53 +0200

    Fixed small bug on HoraMedicion

--- quality/models.py
+++ quality/models.py
@@ -96,4 +96,4 @@ class HoraMedicion(models.Model):
     validacion = models.CharField(max_length=3)
 
     def __str__(self):
-        return f'{str(self.dia_medicion)} - {str(self.tiempo)}'
+        return f'{str(self.dia_medicion)} - {str(self.hora)}'
